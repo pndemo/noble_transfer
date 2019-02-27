067acdcb14cb159a0bb89d4f2b06fdfadb6df0c4
pndemo <ndemopaul1@gmail.com>
2019-02-28 01:18:05 +0300

    Change environment variables location

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,10 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Load the app's custom environment variables
+app_environment_variables = File.join(Rails.root, 'config', 'app_environment_variables.rb')
+load(app_environment_variables) if File.exists?(app_environment_variables)
+
 # Initialize the Rails application.
 Rails.application.initialize!
 
