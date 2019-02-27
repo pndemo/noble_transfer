4736b9226cc4055d3849807e81483a100958a100
pndemo <ndemopaul1@gmail.com>
2019-02-27 22:08:44 +0300

    Add and style homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,7 @@
 class HomeController < ApplicationController
+  def index 
+    if user_signed_in? 
+      @docs = current_user.docs.order("uploaded_file_file_name desc")       
+    end
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Time format
+Time::DATE_FORMATS[:default] = "%d/%m/%Y %l:%M%p"
